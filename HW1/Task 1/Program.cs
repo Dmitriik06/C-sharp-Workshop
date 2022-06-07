5628dbfb9c70095c1268f7de9f933e20834c4bd3
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-07 22:00:04 +0300

    The Task 1 is presented

---
+++ HW1/Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter the first number => ");
+string NumStr1 = Console.ReadLine();
+int Num1 = int.Parse(NumStr1);
+Console.WriteLine("Enter the second number => ");
+string NumStr2 = Console.ReadLine();
+int Num2 = int.Parse(NumStr2);
+
+if (Num1 > Num2)
+{
+    Console.Write($"The First number ({Num1}) is greater than the Second number ({Num2})");
+}
+else
+{
+    if (Num2 > Num1)
+{
+    Console.Write($"The Second number ({Num2}) is greater than the First number ({Num1})");
+}
+else
+{
+    Console.Write($"The numbers are equal");  
+}
+}
