d0eebe5d9dedf7a7e30b55840029c8bd0d3205f0
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-07 23:27:04 +0300

    The Task 3 is presented

---
+++ HW1/Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the random integer number => ");
+string NumStr1 = Console.ReadLine();
+int Num1 = int.Parse(NumStr1);
+
+if ( Num1 % 2 == 0)
+{
+    Console.WriteLine($"The number you entered ({Num1}) is even");
+}
+else
+{
+    Console.WriteLine($"The number you entered ({Num1}) is odd");
+}
