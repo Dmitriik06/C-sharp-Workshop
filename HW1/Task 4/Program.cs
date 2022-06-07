e6e1159817c1b0ef62e08640e22118d9dcba1e4e
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-07 23:55:42 +0300

    The Task 4 is presented

---
+++ HW1/Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter the random natural number => ");
+string NumStr1 = Console.ReadLine();
+int Num1 = int.Parse(NumStr1);
+int i = 2;
+
+while ( i < Num1)
+{
+    if ( i + 1 == Num1)
+    {
+        Console.Write($"{i}");
+        i = i + 2;
+    }
+    else
+    {
+        Console.Write($"{i}, ");
+        i = i + 2;
+    }
+}
+if (Num1 % 2 == 0)
+{
+    Console.Write(Num1);
+}
