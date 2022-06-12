5bb7a0583d50c205041d3cb94c5670be5f116a26
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-12 14:32:52 +0300

    Homework 2 Task 3 is presented

---
+++ HW2/Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter a number => ");
+string NumStr = Console.ReadLine();
+int Num = int.Parse(NumStr);
+int NumLength = NumStr.Length;
+if (Num < 0)
+{
+    Num = -Num;
+    NumLength = NumLength - 1;
+}
+if (Num < 100)
+{
+    Console.WriteLine($"Number {Num} does not contain third digit, try again");
+}
+else
+{
+    while (NumLength > 3)
+    {
+        Num = Num / 10;
+        NumLength = NumLength - 1;
+    }
+    Num = Num % 10;
+    Console.WriteLine($"{Num}");
+}
