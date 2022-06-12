4fe8701b9881772d3b69a5064789864d630464e8
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-12 15:08:06 +0300

    Homework 2 Task 4 is presented

---
+++ HW2/Task 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please enter the number of day of the week (1-7) => ");
+string DayStr = Console.ReadLine();
+int Day = int.Parse(DayStr);
+if (Day < 1 || Day > 7)
+{
+    Console.WriteLine("Incorrect input, please try again");
+}
+else
+{
+    if (Day == 6 || Day == 7)
+    {
+        Console.WriteLine($"The day number {Day} is a Holiday!");
+    }
+    else
+    {
+        Console.WriteLine($"The day number {Day} is not a Holiday!");
+    }
+}
