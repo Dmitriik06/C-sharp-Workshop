6a0ab1d003c79445a2b87b961ebbc5d5604b03b8
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-12 13:42:51 +0300

    Homework 2 Task 1 is presented

---
+++ HW2/Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter a three-digit number => ");
+string NumStr = Console.ReadLine();
+int Num = int.Parse(NumStr);
+if (Num < 0)
+{
+    Num = -Num;
+}
+if (Num < 100 || Num > 999)
+{
+    Console.WriteLine("Incorrect input. Please try again.");
+}
+else
+{
+    Console.WriteLine($"{Num % 100 / 10}");
+}
