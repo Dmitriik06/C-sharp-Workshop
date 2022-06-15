b7cbf569ad5fee15b4215c499cd2156129818dd6
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-15 23:23:19 +0300

    Homework 3 Task 3 is presented

---
+++ HW3/Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿int prompt(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int x = prompt("Enter number");
+
+for (int i = 1; i <= x; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
