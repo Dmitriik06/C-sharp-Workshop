2b2879cdd4e4e11a0f1bd57f86c6e26a224482a7
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-15 22:50:23 +0300

    Homework 3 Task 1 is presented

---
+++ HW3/Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter number => ");
+string Pal = Console.ReadLine();
+int Len = Pal.Length;
+
+bool q = true;
+
+for (int i = 0; i <= Len / 2; i++)
+{
+    if (Pal[i] != Pal[Len - 1 - i])
+    {
+        Console.WriteLine($"{Pal} is not a Palindrome");
+        q = false;
+        break;
+    }
+}
+
+if (q == true)
+{
+    Console.WriteLine($"{Pal} is a Palindrome");
+}
