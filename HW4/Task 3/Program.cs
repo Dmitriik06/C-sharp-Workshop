658bf7f45d47fc1564327267b26e02ae0973725c
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-18 21:26:55 +0300

    Homework 4 Task 3 is presented

---
+++ HW4/Task 3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3:
+// Напишите программу, которая задаёт массив из 8 элементов случайными числами (от 0 до 9999) и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+}
+
+int[] OctoRandom()
+{
+    int[] OctoArray = new int[8];
+
+    for (int i = 0; i < OctoArray.Length; i++)
+    {
+        OctoArray[i] = new Random().Next(0, 10000);
+    }
+
+    return OctoArray;
+}
+
+int[] NewArray = OctoRandom();
+
+PrintArray(NewArray);
