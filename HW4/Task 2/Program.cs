fcd0029b54ae62323bcb62dcb87ed1f1d44ed673
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-18 20:59:55 +0300

    Homework 4 Task 2 is presented

---
+++ HW4/Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2:
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int DigitSum(int Num)
+{
+    int result = 0;
+
+    while(Num != 0)
+    {
+        result = result + Num % 10;
+        Num = Num / 10;
+    }
+
+    return result;
+}
+
+int Number = prompt("Enter number => ");
+int answer = DigitSum(Number);
+
+Console.WriteLine($"The sum of digits of numer {Number} is {answer}");
