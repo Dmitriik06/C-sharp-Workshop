f23daa2f4e74915507a6e5da37f1a8f8c631609c
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-21 23:04:28 +0300

    Homework 5 Task 1 is presented

---
+++ HW5/Task 1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1:
+// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] InitArray(int arrLen)
+{
+    int[] Arr = new int [arrLen];
+    return Arr;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(int element in arr)
+    {
+        Console.Write($"{element} ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillRandArr(int[] arr, int min, int max)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+int CountEven(int[] arr)
+{
+    int i = 0;
+    foreach(int element in arr)
+    {
+        if (element % 2 == 0)
+        {
+            i++;
+        }
+    }
+    Console.WriteLine($"There is/are {i} even numbers in array");
+    return i;
+}
+
+int[] NewArray = InitArray(100);
+
+FillRandArr(NewArray, 100, 999);
+
+PrintArray(NewArray);
+
+CountEven(NewArray);
