26607a3c776b79fe823b954f07d9a1ed2334c98d
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-26 15:28:27 +0300

    Homework 6 Task 1 is presented

---
+++ HW6/Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1:
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int CountPositiveNumbers()
+{
+    int Number = 0;
+    int Positive = 0;
+    
+    int M = Prompt("How much numbers do you want to add?");
+    
+    for (int i = 0; i < M; i++)
+    {
+        Number = Prompt("Enter number =>");
+        if (Number > 0)
+           { Positive++; }
+    }
+    return Positive;
+}
+
+int EnteredPositives = CountPositiveNumbers();
+
+Console.WriteLine($"You entered {EnteredPositives} positive number/numbers");
