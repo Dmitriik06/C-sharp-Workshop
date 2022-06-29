3514395771a53ef81e99e5f51eb60739d2ec1b3d
dmitry kuznetsov <dmitriik06@bk.ru>
2022-06-29 22:23:18 +0300

    Homework 7 Task 1 is presented

---
+++ HW7/Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1:
+// Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+double[,] InitArrayMxN(int m, int n)
+{
+    double[,] ArrayMxN = new double[m, n];
+    return ArrayMxN;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        { Console.Write($"{array[i, j]:F3} "); }
+        Console.WriteLine();
+    }
+}
+
+double[,] Fill2DArray(double[,] array, int min, int max)
+{
+    double number;
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            number = rnd.Next(min, max) + rnd.NextDouble();
+            array[i, j] = number;
+        }
+    }
+    return array;
+}
+
+double[,] NewArrayMxN = InitArrayMxN(10, 10);
+
+NewArrayMxN = Fill2DArray(NewArrayMxN, 0, 10);
+
+Print2DArray(NewArrayMxN);
