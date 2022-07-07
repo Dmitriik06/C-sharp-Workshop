4c1796dd4bba6898e00f65662b029fa6f39ea794
dmitry kuznetsov <dmitriik06@bk.ru>
2022-07-07 18:40:01 +0300

    Homework 9 Task 2 is presented

---
+++ HW9/Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2:
+// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int SumEvenNums(int M, int N)
+{
+    int Sum = 0;
+    while (M < 0) //только натуральные
+    {
+        M++;
+    }
+    if (M <= N)
+    {     
+        Sum = M + SumEvenNums(++M, N);
+    }
+    return Sum;
+}
+
+Console.WriteLine(SumEvenNums(1, 15));
